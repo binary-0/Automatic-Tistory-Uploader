81e56ebcdf719e0be7e27f4e8a5afee82b93dcf6
Hyo je Sung <77618270+Heukma@users.noreply.github.com>
2023-08-18 16:42:06 +0900

    Get arguments from os, post README.md

--- src/main.py
+++ src/main.py
@@ -4,33 +4,62 @@ import json
 import os
 import readRepoCommits
 from git import Repo
+import markdown
 
 #System arguments
 #python {File.py} {access token} {blog name} {repository name}
 #arguments = sys.argv
 
-# access_token = os.environ["INPUT_ACCESSTOKEN"]
-# blogName = os.environ["INPUT_BLOGNAME"]
-# repoName = os.environ['GITHUB_REPOSITORY']
-# repoName = str(repoName)
+blogAccessToken = os.environ["INPUT_ACCESSTOKEN"]
+blogName = os.environ["INPUT_BLOGNAME"]
+Repo_Name = os.environ['GITHUB_REPOSITORY']
+repoName = str(Repo_Name).split('/')[0]
+repoOwner = str(Repo_Name).split('/')[1]
+gitAccessToken = os.environ["INPUT_GITHUBTOKEN"]
 
-arguments = sys.argv
-blogAccessToken = arguments[1]
-blogName = arguments[2]
-repoName = arguments[3]
-repoOwner = arguments[4]
-gitAccessToken = arguments[5]
+# arguments = sys.argv
+# blogAccessToken = arguments[1]
+# blogName = arguments[2]
+# repoName = arguments[3]
+# repoOwner = arguments[4]
+# gitAccessToken = arguments[5]
 
 global contents
 contents = ''
+
+def convert_md_to_html(md_content):
+    html_content = markdown.markdown(md_content)
+    return html_content
+
+def get_readme_from_github(owner, repo):
+    url = f'https://api.github.com/repos/{owner}/{repo}/contents/README.md'
+    
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        readme_data = response.json()
+        
+        # Check if the response contains a 'content' key
+        if 'content' in readme_data:
+            import base64
+            readme_content = base64.b64decode(readme_data['content']).decode('utf-8')
+            return readme_content
+        else:
+            print("README content not found in the response.")
+            return None
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching README from GitHub: {e}")
+        return None
 
 def contents_generator():
     #ReadMe 읽고 맨 위에 쓰고
     global contents
     
+    contents += convert_md_to_html(get_readme_from_github(repoOwner , repoName))
+    
     commits = readRepoCommits.get_commits(repoOwner, repoName, gitAccessToken)
     commitCounter = 1
-
+    
     for commit in commits:
         if commit["commit"]["message"] != '':
             contents += '<p>'
